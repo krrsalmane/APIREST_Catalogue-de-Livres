938a6fe0f73777efb7c84f4ffd0f693eee4fd892
krrsalmane <karrounsalmane@gmail.com>
2025-08-07 11:35:45 +0100

    create entity

---
+++ src/main/java/com/s/apirest_cataloguedelivres/ApiRestCatalogueDeLivresApplication.java
@@ -0,0 +1,12 @@
+package com.s.apirest_cataloguedelivres;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCatalogueDeLivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCatalogueDeLivresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s/apirest_cataloguedelivres/ApiRestCatalogueDeLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s.apirest_cataloguedelivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCatalogueDeLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
