252a422db3f462556713f72870a9b44adb8b441d
krrsalmane <karrounsalmane@gmail.com>
2025-08-07 11:56:36 +0100

    create getters and setters

--- src/main/java/com/s/apirest_cataloguedelivres/Entites/Author.java
+++ src/main/java/com/s/apirest_cataloguedelivres/Entites/Author.java
@@ -1,15 +1,70 @@
 package com.s.apirest_cataloguedelivres.Entites;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 public class Author {
     @Id
-    @GeneratedValue
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private  Long id;
     private String title ;
     private String isbn ;
     private String author;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books = new ArrayList<>();
+
+    public Author(Long id, String title, String isbn, String author, List<Book> books) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+        this.books = books;
+    }
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
--- src/main/java/com/s/apirest_cataloguedelivres/Entites/Book.java
+++ src/main/java/com/s/apirest_cataloguedelivres/Entites/Book.java
@@ -1,15 +1,58 @@
 package com.s.apirest_cataloguedelivres.Entites;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 
 
 @Entity
 public class Book {
 @Id
-@GeneratedValue
+@GeneratedValue ( strategy = GenerationType.IDENTITY)
    private Long   id;
    private String name;
    private String biography;
+
+   @ManyToOne
+   @JoinColumn(name = "author_id")
+   private Author author;
+
+   public Book() {
+   }
+
+   public Book(Long id, String name, String biography, Author author) {
+      this.id = id;
+      this.name = name;
+      this.biography = biography;
+      this.author = author;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getBiography() {
+      return biography;
+   }
+
+   public void setBiography(String biography) {
+      this.biography = biography;
+   }
+
+   public Author getAuthor() {
+      return author;
+   }
+
+   public void setAuthor(Author author) {
+      this.author = author;
+   }
 }
